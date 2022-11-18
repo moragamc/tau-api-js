7199de983a6ddbe9946fce05aa82ebbcd91e7c0f
moragamc <christiangeo94@gmail.com>
2022-11-18 15:08:59 -0600

    Primera version de pruebas con JS

---
+++ tests/get.spec.js
@@ -0,0 +1,20 @@
+const app = require('../src/app')
+const request = require('supertest');
+const expect = require('chai').expect;
+
+describe('Get requests', () => {
+    it('get course id', ()=>{
+        request(app)
+        .get('/course/1')
+        .end((err, res)=>{
+            expect(res.body.id).to.be.equal('1');
+        });
+    })
+
+    it('get query param name ', (done) => {
+        request(app)
+        .get('/course')
+        .query({ 'name' :'mocha' })
+        .expect(200, { id:'1', name: 'mocha'}, done);
+    })
+});
